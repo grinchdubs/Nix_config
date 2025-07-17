70bedbd28a8cfab8d45d525bf7934cbdb775bb37
grnch <grnch@grnch.xyz>
2025-07-17 10:44:30 -0400

    first commit

---
+++ configuration.nix
@@ -0,0 +1,228 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      <home-manager/nixos>
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "grnchnix"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  networking.interfaces.eth0.ipv4.addresses = [ {
+    address = "192.191.5.108";
+    prefixLength = 24;
+  } ];
+  networking.defaultGateway = "192.191.5.1";
+  networking.nameservers = [ "192.168.1.42" ];
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = false;
+  services.gnome.gnome-keyring.enable = true;
+  
+  # enable Sway window manager
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
+ 
+  security.polkit.enable = true; 
+ 
+  # Enabling fonts
+  fonts.fontDir.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+  # Tailscale
+  services.tailscale.enable = true;
+
+  # Enable the Flakes feature and the accompanying new nix command-line tool
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Qemu
+  # services.qemuGuest.enable = true;  
+
+  # Set the default editor to vim
+  environment.variables.EDITOR = "vim";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.grnch = {
+    isNormalUser = true;
+    description = "grnch";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    shell = pkgs.fish;
+    packages = with pkgs; [
+      kdePackages.kate
+      thunderbird
+    ];
+  };
+ 
+  environment.variables.MANPAGER = "nvim +Man!";
+
+  # home-manager.users.grnch = { pkgs, ... }: {
+  #   home.packages = with pkgs; [
+  #	neovim
+  #      atool 
+  #	httpie
+  #	telegram-desktop 
+  #   ];
+  #   home.stateVersion = "25.05";
+  # };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Install Apps.
+  programs.firefox.enable = true;
+  programs.fish.enable = true;
+  programs.nix-ld.enable = true;
+  programs.bat.enable = true;  
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  wget
+  fish
+  screen
+  git
+  eza
+  ncdu
+  cargo
+  glances
+  bat
+  fd
+  fzf
+  termshark
+  procs
+  progress
+  ipcalc
+  unp
+  iotop
+  moreutils
+  ripgrep-all
+  mtr
+  mosh
+  sshs
+  lshw
+  duf
+  ranger
+  yazi
+  asciinema
+  oh-my-fish
+  nmap
+  tldr
+  fastfetch
+  home-manager
+  neovim
+  jekyll
+  emacs
+  grim # screenshot functionality
+  slurp # screenshot functionality
+  wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
+  sway
+  mako # notification system developed by swaywm maintainer
+  ];
+
+
+  # Docker Install
+  virtualisation.docker.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = true;
+    settings.PermitRootLogin = "no";
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = true;
+
+}
